d969f46f642e18a441488c1fd871b44982d18ac0
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-22 10:54:49 -0500

    Add files via upload

---
+++ Modulo1-Video8-Constantes/src/DeclaracionCostantes.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class DeclaracionCostantes {
+        public static void main(String args[]){
+            final double pulgadas = 2.55;
+            
+            // No se puede cambiar el valor debido a que es una constante
+            //a= 10;
+            
+            double cm =6.0;
+            
+            double resultado = cm/pulgadas;
+          
+            
+            System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+            
+            //Definir variables en una sola linea de codigo
+            
+            int operador1, operador2, resultado2;
+            
+            operador1 = 8;
+            
+            operador2 = 2;
+            
+            resultado = operador1 + operador2;
+            
+            System.out.println("El resultado es " + resultado);
+            
+       
+    }
+}
