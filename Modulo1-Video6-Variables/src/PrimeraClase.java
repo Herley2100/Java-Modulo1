a3d4caa84e5300a23732366ae753ea69972d6be0
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-22 10:12:09 -0500

    Add files via upload

---
+++ Modulo1-Video6-Variables/src/PrimeraClase.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class PrimeraClase {
+    public static void main(String args[]){
+        System.out.print("Hola Alumnos ");
+    }
+}
---
+++ Modulo1-Video6-Variables/src/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Variables {
+        public static void main(String args[]){
+          int edad =35; //declaracion de variable Edad
+                  
+            System.out.println(edad);
+            /*
+            Se cambia el valor de la variable y se vuelve a imprimir en consola
+            */
+            
+            edad = 75;
+            System.out.println(edad);
+    }
+}
