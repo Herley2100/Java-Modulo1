229d35f23d571c930ffe97873398f8bc9547a5e4
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-22 10:55:41 -0500

    Add files via upload

---
+++ Modulo1-Video8-Ejercicios/src/Ejercicios.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Ejercicios {
+        public static void main(String args[]){
+           
+            final double dolar = 1.137, salario = 2125.70;
+            
+            String nombre = "Manuel";
+            
+            double salario_en_dolar = salario * dolar;
+            
+            System.out.println("El salario de " + nombre + " es: " + salario_en_dolar);
+            
+            
+            
+       
+    }
+}
