f489e952bcdba9ad4ce00acd0bb6b7e333619b59
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-22 11:06:03 -0500

    Add files via upload

---
+++ Modulo1-Video9-Math/src/Calculo_conMath.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Calculo_conMath {
+        public static void main(String args[]){
+        
+            //No se puede almacenar reales en un int
+            //int raiz = Math.PI;
+            
+            double raiz = Math.sqrt(9);
+            
+            System.out.println(raiz);
+
+            
+       
+    }
+}
