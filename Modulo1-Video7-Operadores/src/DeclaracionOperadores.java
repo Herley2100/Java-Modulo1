153a23e48e6444caa52f39e88e35829e0af7ec33
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-22 10:13:39 -0500

    Add files via upload

---
+++ Modulo1-Video7-Operadores/src/DeclaracionOperadores.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class DeclaracionOperadores {
+        public static void main(String args[]){
+            int a = 5;
+            int b;
+            b = 7;
+            
+            int c = a + b;
+            
+            c++;
+            
+            System.out.println(c);
+       
+    }
+}
---
+++ Modulo1-Video7-Operadores/src/Variables.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Variables {
+        public static void main(String args[]){
+            //Creacion de las variables en el ejercicio del Video 6
+            String nombre = "Ana";
+            short edad = 27;
+            float salario = (float) 1800.5;
+            boolean carnet = true;
+            
+            //Imprimir los datos
+            System.out.println(nombre);
+            System.out.println(edad);
+            System.out.println(salario);
+            System.out.println(carnet);
+ 
+    }
+}
