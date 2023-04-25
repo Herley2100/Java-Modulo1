b04b317588ce8e3ee638d1a4373772d3180ce1a6
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-04-25 00:07:49 -0500

    Add files via upload

---
+++ Modulo1-Video12-CadenaStringII/src/Manipula_Cadenas_II.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Manipula_Cadenas_II {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String frase = "Hoy es un hermoso dia para aprender a programar en Java";
+        String resumen = frase.substring(22,55);
+        
+        System.out.println(resumen);
+    }
+    
+}
---
+++ Modulo1-Video12-CadenaStringII/src/Manipula_Cadenas_III.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Daniela
+ */
+public class Manipula_Cadenas_III {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String alumno1, alumno2;
+        alumno1 = "Eduardo";
+        
+        alumno2 = "Eduerdo";
+        if (alumno1.equals(alumno2) == true){
+            System.out.println("Son iguales");
+        } else{
+            System.out.println("Son diferentes");
+        }
+    }
+    
+}
