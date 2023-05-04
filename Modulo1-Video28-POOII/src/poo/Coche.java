77645825680b1a9b3e5efb46287ced3be28a3f21
Herley2100 <131308884+Herley2100@users.noreply.github.com>
2023-05-04 17:03:53 -0500

    Add files via upload

---
+++ Modulo1-Video28-POOII/src/poo/Coche.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Coche {
+    
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    
+    public Coche (){
+        this.ruedas = 4;
+        this.largo = 10;
+        this.ancho = 10;
+        this.motor = 100;
+        this.peso = 2000;
+    }
+
+    public int getRuedas() {
+        return ruedas;
+    }
+    
+    
+    
+}
